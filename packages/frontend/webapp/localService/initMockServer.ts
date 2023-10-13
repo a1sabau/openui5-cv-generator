19fc26ca3f61e041743b4eab3ee7f3564a8807ea
Andrei Sabau <andrei@asabau.com>
2023-10-13 17:12:06 +0300

    control whether to use odata or mock data

---
+++ packages/frontend/webapp/localService/initMockServer.ts
@@ -0,0 +1,25 @@
+import Log from 'sap/base/Log'
+import MockServer from 'sap/ui/core/util/MockServer'
+
+const enableMockServer = false
+
+if (enableMockServer) {
+  // create
+  var oMockServer = new MockServer({
+    rootUri: '/browse/',
+  })
+
+  // simulate against the metadata and mock data
+  oMockServer.simulate('./localService/metadata.xml', {
+    sMockdataBaseUrl: './localService/mockdata',
+    bGenerateMissingMockData: true,
+  })
+
+  // start
+  oMockServer.start()
+
+  Log.info('Running the app with mock data')
+}
+
+// initialize the embedded component on the HTML page
+sap.ui.require(['sap/ui/core/ComponentSupport'])
