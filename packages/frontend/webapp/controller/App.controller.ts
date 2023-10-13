c4e5a54176627dd61c4ee0f44b405da25c74b6fe
Andrei Sabau <andrei@asabau.com>
2023-10-13 17:03:27 +0300

    main/app view and controllers, config files

---
+++ packages/frontend/webapp/controller/App.controller.ts
@@ -0,0 +1,34 @@
+import BaseController from './BaseController'
+import MockServer from 'sap/ui/core/util/MockServer'
+import Log from 'sap/base/Log'
+
+/**
+ * @namespace ui5.cv.controller
+ */
+export default class App extends BaseController {
+  public onInit(): void {
+    // this.runMockServer()
+
+    console.log('App controller onInit')
+    // apply content density mode to root view
+    this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass())
+  }
+
+  public runMockServer(): void {
+    // create
+    var oMockServer = new MockServer({
+      rootUri: '/browse/',
+    })
+
+    // simulate against the metadata and mock data
+    oMockServer.simulate('../localService/metadata.xml', {
+      sMockdataBaseUrl: '../localService/mockdata',
+      bGenerateMissingMockData: true,
+    })
+
+    // start
+    oMockServer.start()
+
+    Log.info('Running the app with mock data')
+  }
+}
