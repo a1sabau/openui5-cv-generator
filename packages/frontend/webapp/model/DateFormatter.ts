598d2de4a6d91bc5175c0e0ae44833d461b8b4db
Andrei Sabau <andrei@asabau.com>
2023-10-13 17:10:22 +0300

    date related formaters

---
+++ packages/frontend/webapp/model/DateFormatter.ts
@@ -0,0 +1,17 @@
+/**
+ * @namespace ui5.cv.model
+ */
+export default class DateFormatter {
+  public static formatInterval(startDate: Date, endDate: Date): string {
+    const dateIntervalVals = [
+      startDate.toLocaleString(undefined, { year: 'numeric', month: '2-digit' }),
+      endDate.toLocaleString(undefined, { year: 'numeric', month: '2-digit' }),
+    ]
+
+    return dateIntervalVals.join(' - ')
+  }
+
+  public static formatDate(date: Date): string {
+    return date.toLocaleString(undefined, { year: 'numeric', month: '2-digit' })
+  }
+}
