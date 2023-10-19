6d21bff224296922261cf1feb5c5d26b0bd8a475
Andrei Sabau <andrei@asabau.com>
2023-10-19 18:40:24 +0300

    code formating update and cleanup

--- packages/frontend/webapp/controller/App.controller.ts
+++ packages/frontend/webapp/controller/App.controller.ts
@@ -1,34 +1,31 @@
-import BaseController from './BaseController'
-import MockServer from 'sap/ui/core/util/MockServer'
-import Log from 'sap/base/Log'
+import BaseController from './BaseController';
+import MockServer from 'sap/ui/core/util/MockServer';
+import Log from 'sap/base/Log';
 
 /**
  * @namespace ui5.cv.controller
  */
 export default class App extends BaseController {
   public onInit(): void {
-    // this.runMockServer()
-
-    console.log('App controller onInit')
     // apply content density mode to root view
-    this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass())
+    this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
   }
 
   public runMockServer(): void {
     // create
     var oMockServer = new MockServer({
       rootUri: '/browse/',
-    })
+    });
 
     // simulate against the metadata and mock data
     oMockServer.simulate('../localService/metadata.xml', {
       sMockdataBaseUrl: '../localService/mockdata',
       bGenerateMissingMockData: true,
-    })
+    });
 
     // start
-    oMockServer.start()
+    oMockServer.start();
 
-    Log.info('Running the app with mock data')
+    Log.info('Running the app with mock data');
   }
 }
--- packages/frontend/webapp/controller/Main.controller.ts
+++ packages/frontend/webapp/controller/Main.controller.ts
@@ -1,18 +1,14 @@
-import MessageBox from 'sap/m/MessageBox'
-import BaseController from './BaseController'
-import ODataModel from 'sap/ui/model/odata/v4/ODataModel'
-import Log from 'sap/base/Log'
-import MockServer from 'sap/ui/core/util/MockServer'
+import BaseController from './BaseController';
 
 /**
  * @namespace ui5.cv.controller
  */
 export default class Main extends BaseController {
   public onInit(): void {
-    this.getView()?.bindElement({ path: `/Cv(1)` })
+    this.getView()?.bindElement({ path: `/Cv(1)` });
   }
 
   public switchToNextjsCV(): void {
-    window.location.href = '/'
+    window.location.href = '/';
   }
 }
